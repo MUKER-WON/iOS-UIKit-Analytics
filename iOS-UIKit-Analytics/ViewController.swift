1bba528e104a5eb1aa7072734a30fe78ec59def6
MUKER-WON <muker.dev@gmail.com>
2024-05-15 16:46:58 +0900

    [Feat] LogEvent Test

--- iOS-UIKit-Analytics/ViewController.swift
+++ iOS-UIKit-Analytics/ViewController.swift
@@ -6,12 +6,88 @@
 //
 
 import UIKit
+import FirebaseAnalytics
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private let navigationCountLabel: UILabel = {
+        var label = UILabel()
+        label.text = "0"
+        label.font = .systemFont(ofSize: 100)
+        return label
+    }()
+    
+    private lazy var buttonConfig: UIButton.Configuration = {
+        var config = UIButton.Configuration.filled()
+        config.title = "다음 페이지"
+        return config
+    }()
+    
+    private lazy var nextPageAction = UIAction(
+        image: .strokedCheckmark,
+        handler: { [weak self] _ in
+            self?.navigationController?.pushViewController(
+                ViewController(
+                    count: String(Int((self?.navigationCountLabel.text)!)! + 1)
+                ),
+                animated: true
+            )
+              
+            Analytics.logEvent("nextPageAction", parameters: nil)
+        }
+    )
+    
+    init(count naviagtionCount: String) {
+        self.navigationCountLabel.text = naviagtionCount
+        super.init(nibName: nil, bundle: nil)
+        
+        Analytics.logEvent("View Init", parameters: ["init": #file])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var nextPageButton: UIButton = {
+        var button = UIButton(
+            configuration: buttonConfig,
+            primaryAction: nextPageAction
+        )
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+    }
+    
+    private func configureUI() {
+        
+        view.backgroundColor = .white
+        
+        [navigationCountLabel, nextPageButton]
+            .forEach {
+                $0.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview($0)
+            }
+        NSLayoutConstraint.activate([
+            
+            navigationCountLabel.centerXAnchor.constraint(
+                equalTo: view.centerXAnchor
+            ),
+            navigationCountLabel.centerYAnchor.constraint(
+                equalTo: view.centerYAnchor
+            ),
+            
+            nextPageButton.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                constant: -10
+            ),
+            nextPageButton.centerXAnchor.constraint(
+                equalTo: view.centerXAnchor
+            ),
+        ])
+        
     }
 
 
